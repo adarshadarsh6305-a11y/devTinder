6b6e93f81e9c1038de6d1b9b34b50d446a4418e7
adarshadarsh6305-a11y <adarshadarsh6305@gmail.com>
2026-03-02 10:14:32 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 const express=require("express");
 const app=express();
-app.use("/home",(req,res)=>{
+
+
+app.get("/user",(req,res)=>{
+    res.send({firstname:"M",lastname:"Adarsh"});
+})
+app.post("/user",(req,res)=>{
+    //save changes to db
+    res.send("DB changed successfully");
+})
+app.delete("/user",(req,res)=>{
+    res.send("deleted successfully");
+});
+app.use("/user",(req,res)=>{
     res.send("hello from home");
 });
-app.use("/title",(req,res)=>{
-    res.send("devTinder project");
-});
-app.use((req,res)=>{
-    res.send("hello from server adarsh");
-});
+
+
+
+
 app.listen(535,()=>{
     console.log("server is listening at 535");
 }
