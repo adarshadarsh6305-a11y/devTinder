594f6ea068f9419ac3755a06eeb8408e648dd1fd
adarshadarsh6305-a11y <adarshadarsh6305@gmail.com>
2026-03-02 10:59:14 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,24 +2,12 @@ const express=require("express");
 const app=express();
 
 
-app.get("/user",(req,res)=>{
+app.get("/user/:userid/:name",(req,res)=>{
+    console.log(req.params);
     res.send({firstname:"M",lastname:"Adarsh"});
-})
-app.post("/user",(req,res)=>{
-    //save changes to db
-    res.send("DB changed successfully");
-})
-app.delete("/user",(req,res)=>{
-    res.send("deleted successfully");
 });
-app.use("/user",(req,res)=>{
-    res.send("hello from home");
-});
-
-
-
 
 app.listen(535,()=>{
     console.log("server is listening at 535");
 }
-)
+);
