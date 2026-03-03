6d1ca96ead486682420e428f23a4ae308a4b3b4d
adarshadarsh6305-a11y <adarshadarsh6305@gmail.com>
2026-03-03 22:51:24 +0530

    wild-card-error handling

--- src/app.js
+++ src/app.js
@@ -2,11 +2,18 @@ const express=require("express");
 const app=express();
 
 app.get("/user",(req,res)=>{
-
-   throw new error;
+try{
+   throw new Error("sjknj");
+   res.send("error occured");
+}
+catch(err){
+    res.status(404).send("error occured");
+}
 });
 app.use("/",(err,req,res,next)=>{
+    if(err){
     res.status(404).send("something went wrong");
+    }
 })
 
 
